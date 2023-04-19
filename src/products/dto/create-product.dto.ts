7c4f1871157535bcb4755c252ba23737df838628
sebasGZA <sebassga34@gmail.com>
2023-04-19 17:23:51 -0500

    CreateProductDto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,42 @@
-export class CreateProductDto {}
+import {
+  IsArray,
+  IsIn,
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @MinLength(1)
+  title: string;
+
+  @IsOptional()
+  @IsNumber()
+  @IsPositive()
+  price?: number;
+
+  @IsString()
+  @IsPositive()
+  @IsOptional()
+  description?: string;
+
+  @IsString()
+  @IsOptional()
+  slug?: string;
+
+  @IsInt()
+  @IsPositive()
+  @IsOptional()
+  stock?: string;
+
+  @IsString({ each: true })
+  @IsArray()
+  sizes: string[];
+
+  @IsIn(['men', 'women', 'kid', 'unisex'])
+  gender: string;
+}
