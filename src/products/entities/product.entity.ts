21c4e5ab0a4309a811667772aae2aec87ef4e836
sebasGZA <sebassga34@gmail.com>
2023-04-20 11:53:27 -0500

    beforeUpdate

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,4 +1,10 @@
-import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class Product {
@@ -45,6 +51,17 @@ export class Product {
       this.slug = this.title;
     }
 
-    this.slug.toLowerCase().replaceAll(' ', '_').replaceAll("'", '');
+    this.slug = this.slug
+      .toLowerCase()
+      .replaceAll(' ', '_')
+      .replaceAll("'", '');
+  }
+
+  @BeforeUpdate()
+  checkSlugUpdate() {
+    this.slug = this.slug
+      .toLowerCase()
+      .replaceAll(' ', '_')
+      .replaceAll("'", '');
   }
 }
