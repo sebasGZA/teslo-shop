01c9dc51ce39f653582170b6f71c5e3992a1970f
sebasGZA <sebassga34@gmail.com>
2023-04-20 11:38:18 -0500

    queryBuilder

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -42,7 +42,12 @@ export class ProductsService {
     if (isUUID(term)) {
       product = await this.productRepository.findOneBy({ id: term });
     } else {
-      product = await this.productRepository.findOneBy({ slug: term });
+      // product = await this.productRepository.findOneBy({ slug: term });
+      const queryBuilder = this.productRepository.createQueryBuilder();
+      product = await queryBuilder.where(`UPPER(title)=:title or slug=:slug`, {
+        title: term.toUpperCase(),
+        slug: term.toLowerCase(),
+      }).getOne();
     }
 
     if (!product)
