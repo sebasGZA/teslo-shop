5bb22b64f3ec011594ccc3e7f3193c681af01a06
sebasGZA <sebassga34@gmail.com>
2023-04-20 10:26:59 -0500

    beforeInsert

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Product {
@@ -38,4 +38,13 @@ export class Product {
 
   @Column('text')
   gender: string;
+
+  @BeforeInsert()
+  checkSlugInsert() {
+    if (!this.slug) {
+      this.slug = this.title;
+    }
+
+    this.slug.toLowerCase().replaceAll(' ', '_').replaceAll("'", '');
+  }
 }
