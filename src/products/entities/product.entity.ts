c5e5a6b8525f91d805870367e76c0171086c550a
sebasGZA <sebassga34@gmail.com>
2023-04-27 14:47:31 -0500

    Create ProductImage

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -58,7 +58,7 @@ export class Product {
     (productImage) => productImage.product,
     { cascade: true }
   )
-  images?: ProductImage;
+  images?: ProductImage[];
 
   @BeforeInsert()
   checkSlugInsert() {
