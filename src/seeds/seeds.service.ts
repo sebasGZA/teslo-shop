fcee57f1651e0adeb467dbed3866d799d0ea1b64
sebasGZA <sebassga34@gmail.com>
2023-04-27 16:31:45 -0500

    inster many products seeds

--- src/seeds/seeds.service.ts
+++ src/seeds/seeds.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ProductsService } from 'src/products/products.service';
+import { initialData } from './data/seed-data';
 
 @Injectable()
 export class SeedsService {
@@ -13,6 +14,15 @@ export class SeedsService {
 
   private async insertNewProducts() {
     await this.productsService.deleteAllProducts();
+    const seedProducts = initialData.products;
+    const insertPromises = [];
+    seedProducts.forEach(product => {
+      insertPromises.push(
+        this.productsService.create(product),
+      )
+    });
+
+    await Promise.all(insertPromises);
     return true
   }
 }
