f210ca30d494c02f709ac34319c54f9d2971d409
sebasGZA <sebassga34@gmail.com>
2024-09-29 16:00:25 -0500

    insert data using typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,6 +12,7 @@ import { ProductsModule } from './products/products.module';
       port: +process.env.DB_PORT,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       autoLoadEntities: true,
       synchronize: true,
     }),
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,11 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+    try {
+      const product = this.productRepository.create(createProductDto);
+      const productCreated = await this.productRepository.save(product);
+      return productCreated;
+    } catch (error: any) {
+      throw new InternalServerErrorException('help');
+    }
   }
 
   findAll() {
