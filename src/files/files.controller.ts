a2590bbd141f957c90d5cc74a6da7bed81f91bc7
sebasGZA <sebassga34@gmail.com>
2024-09-29 21:26:26 -0500

    serves file in a cotrolled way

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,18 +1,32 @@
 import {
   BadRequestException,
   Controller,
+  Get,
+  Param,
   Post,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
-import { FilesService } from './files.service';
-import { FileInterceptor } from '@nestjs/platform-express';
-import { fileFilter, fileNamer } from './helpers';
 import { diskStorage } from 'multer';
+import { Response } from 'express';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+import { FilesService } from './files.service';
+import { fileFilter, fileNamer } from './helpers';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
+
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() response: Response,
+    @Param('imageName') imageName: string,
+  ) {
+    const path = this.filesService.getStaticProductImage(imageName);
+    response.sendFile(path);
+  }
 
   @Post('product')
   @UseInterceptors(
@@ -27,8 +41,9 @@ export class FilesController {
   )
   uploadProductImage(@UploadedFile() file: Express.Multer.File) {
     if (!file) throw new BadRequestException('File is required');
+    const secureUrl = `${file.filename}`;
     return {
-      fileName: file.originalname,
+      secureUrl,
     };
   }
 }
