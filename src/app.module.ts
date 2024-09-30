8e98333b0b18d395754d77aeae0b09311ec84280
sebasGZA <sebassga34@gmail.com>
2024-09-29 19:07:05 -0500

    file upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsModule } from './products/products.module';
 import { SharedModule } from './shared/shared.module';
 import { SeedModule } from './seed/seed.module';
+import { FilesModule } from './files/files.module';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { SeedModule } from './seed/seed.module';
     ProductsModule,
     SharedModule,
     SeedModule,
+    FilesModule,
   ],
 })
 export class AppModule {}
