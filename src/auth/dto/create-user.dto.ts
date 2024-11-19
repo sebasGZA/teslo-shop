97ea4e30a936b843ba13eb0e48cc16ffde4977f3
sebasGZA <sebassga34@gmail.com>
2024-11-19 15:16:31 -0500

    create user

---
+++ src/auth/dto/create-user.dto.ts
@@ -0,0 +1,28 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  @MaxLength(50)
+  @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'The password must have a Uppercase, lowercase letter and a number',
+  })
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  fullName: string;
+}
